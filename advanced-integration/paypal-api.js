23ff334108ded85aa31fe7298145db1f156b3c09
Yoshihiro Watanabe <y15e@qq.com>
2023-01-26 11:17:40 +0900

    Update paypal-api.js

--- advanced-integration/paypal-api.js
+++ advanced-integration/paypal-api.js
@@ -6,7 +6,7 @@ const base = `https://${API_HOST}`;
 
 // call the create order method
 export async function createOrder() {
-  const purchaseAmount = "100.00"; // TODO: pull prices from a database
+  const purchaseAmount = "10"; // TODO: pull prices from a database
   const accessToken = await generateAccessToken();
   const url = `${base}/v2/checkout/orders`;
   const response = await fetch(url, {
@@ -20,7 +20,7 @@ export async function createOrder() {
       purchase_units: [
         {
           amount: {
-            currency_code: "USD",
+            currency_code: "JPY",
             value: purchaseAmount,
           },
         },
