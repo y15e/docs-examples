c5d145aeb42623891370067b435903db10ca0598
Yoshihiro Watanabe <ywatanabe@paypal.com>
2025-01-15 15:50:06 +0900

    disable sourcemap

--- advanced-integration/v2/client/html/vite.config.js
+++ advanced-integration/v2/client/html/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: false
+  },
   plugins: [],
   envDir: "../",
   envPrefix: "PAYPAL",
