32226dc04e947f7377a5dd2bc6043a35c07a835b
Yoshihiro Watanabe <y15e@qq.com>
2026-04-21 15:04:51 +0900

    Update app.js

--- standard-integration/client/html/src/app.js
+++ standard-integration/client/html/src/app.js
@@ -16,17 +16,7 @@ window.paypal
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-          },
-          // use the "body" param to optionally pass additional order information
-          // like product ids and quantities
-          body: JSON.stringify({
-            cart: [
-              {
-                id: "YOUR_PRODUCT_ID",
-                quantity: "YOUR_PRODUCT_QUANTITY",
-              },
-            ],
-          }),
+          }
         });
 
         const orderData = await response.json();
