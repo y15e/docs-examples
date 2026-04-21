127dc242a3064c3a749944c2c5314dad10d061ab
Yoshihiro Watanabe <y15e@qq.com>
2026-04-21 14:47:08 +0900

    Update vite.config.js

--- standard-integration/client/html/vite.config.js
+++ standard-integration/client/html/vite.config.js
@@ -7,10 +7,10 @@ export default defineConfig({
   envPrefix: "PAYPAL",
   root: "src",
   server: {
-    port: 3000,
+    port: process.env.PORT || 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: process.env.SERVER_HOST || "http://localhost:8080",
         changeOrigin: true,
         secure: false,
       },
