03d8562e8f4b1d6a365b4c50323de45d01e3ec11
Yoshihiro Watanabe <y15e@qq.com>
2026-05-10 15:32:15 +0900

    Update app.js

--- standard-integration/client/html/src/app.js
+++ standard-integration/client/html/src/app.js
@@ -9,7 +9,7 @@ const buttons = paypal.Buttons({
       amount: 100,
     },
     appSwitchWhenAvailable: true,
-    browserContext: 'browser',
+    //browserContext: 'browser',
     async createOrder() {
       try {
         const response = await fetch("https://standard-server-qi5l.onrender.com/api/orders", {
@@ -95,8 +95,11 @@ const buttons = paypal.Buttons({
       }
     },
 });
-
-buttons.render("#paypal-button-container");
+if (buttons.hasReturned()) {
+  buttons.resume();
+} else {
+  buttons.render("#paypal-button-container");
+}
 
 // Example function to show a result to the user. Your site's UI library can be used instead.
 function resultMessage(message) {
