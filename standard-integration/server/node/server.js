31dd289c136746c20862481adcc634e989e3ba1f
Yoshihiro Watanabe <y15e@qq.com>
2026-08-04 11:28:51 +0900

    Update PayPal order configuration

--- standard-integration/server/node/server.js
+++ standard-integration/server/node/server.js
@@ -20,49 +20,23 @@ app.use(bodyParser.json());
 const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PORT = 8080, ENV } = process.env;
 
 const order = {
-    "intent": "CAPTURE",
-    "payment_source": {
-        "paypal": {
-            "experience_context": {
-                "brand_name": "Brand name test",
-                "shipping_preference": "NO_SHIPPING",
-                "landing_page": "LOGIN",
-                "payment_method_preference": "IMMEDIATE_PAYMENT_REQUIRED",
-                "return_url": "https://ynabe.com/dmz/ApmConnector/E41AFBFC6C9F8C94021C60BC5715019B",
-                "cancel_url": "https://ynabe.com/dmz/ApmConnector/E41AFBFC6C9F8C94021C60BC5715019B",
-                "user_action": "PAY_NOW",
-                "app_switch_context": {
-                    "mobile_web": {
-                        "return_flow": "AUTO",
-                        "buyer_user_agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.6 Mobile/15E148 Safari/604.1"
-                    }
-                }
-            }
-        }
-    },
-    "purchase_units": [
-        {
-            "description": "BA Description",
-            "amount": {
-                "value": "1.00",
-                "currency_code": "USD"
-            },
-            "shipping": {
-                "address": {
-                    "address_line_1": "xxxxxx",
-                    "admin_area_2": "Ontario",
-                    "postal_code": "M6T 1J1",
-                    "country_code": "US"
-                },
-                "name": {
-                    "full_name": "xxxxxx"
-                }
-            },
-            "custom_id": "asdf",
-            "invoice_id": "asdf-d399-48d7-8ea5-41f89619d2fa6",
-            "soft_descriptor": "test Descriptor"
-        }
-    ]
+  "intent": "CAPTURE",
+  "purchase_units": [
+    {
+      "amount": {
+        "currency_code": "USD",
+        "value": "1.23"
+      }
+    }
+  ],
+  "payment_source": {
+    "paypal": {
+      "experience_context": {
+        "return_url": "https://standard-client.onrender.com/",
+        "cancel_url": "https://standard-client.onrender.com/"
+      }
+    }
+  }
 }
 
 const client = new Client({
